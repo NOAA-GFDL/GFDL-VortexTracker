f5f35af609e34117a73a9766407a5960554a5624
Caitlyn McAllister <caitlyn.mcallister@noaa.gov>
2024-07-16 14:59:35 +0000

    adds intel/2022, adds ncdump path, adds nco and cdo, ands grib modules

--- modulefile-setup/hera-setup.sh
+++ modulefile-setup/hera-setup.sh
@@ -1,5 +1,7 @@
 # set up module environment on hera
 
+module load intel//2022.1.2
+
 module use /scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core/
 module load stack-intel
 
@@ -14,3 +16,13 @@ module load g2
 module load g2tmpl
 module load w3emc
 module load w3nco
+
+module load nco
+module load cdo
+
+export ncdump=/apps/netcdf/4.7.0/intel/18.0.5.274/bin/ncdump
+
+# for grib data
+module load grib-util
+module load wgrib/1.8.1.0b
+module load wgrib2/2.0.8
