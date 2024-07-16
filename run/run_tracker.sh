63cca06fcc83f09c55713e34acf2a7f4f3a1a77c
mcallic2 <caitlyn.mcallister@noaa.gov>
2024-07-16 09:57:58 -0400

    automatically load modules

--- run/run_tracker.sh
+++ run/run_tracker.sh
@@ -17,23 +17,9 @@ export PS4=' + run_tracker.sh line $LINENO: '
 module purge
 . /usr/local/Modules/5.1.1/init/ksh
 
-module load intel-oneapi-compilers
-
-module load hdf5
-module load netcdf-c
-module load netcdf-fortran
-
-module load libpng
-module load jasper
-module load zlib
-module load g2
-module load g2tmpl
-module load bacio
-module load w3emc
-module load w3nco
-
-module load nco
-module load cdo
+# loads any module/packages needed for cmake build
+cd ../modulefiles
+source modulefile.$target
 
 echo " "
 module list
