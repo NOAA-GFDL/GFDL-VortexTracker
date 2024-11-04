3514b4e55777660a251051615e6b856edefa6a89
Caitlyn McAllister <caitlyn.mcallister@noaa.gov>
2024-11-04 15:18:47 -0500

    changes intel version & removes LD_preload env variable

--- code/modulefile-setup/gaea-setup.sh
+++ code/modulefile-setup/gaea-setup.sh
@@ -1,7 +1,7 @@
 # set up module environment on gaea
 
 module use /ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core
-module load stack-intel/2023.1.0
+module load stack-intel/2023.2.0
 
 module load cray-hdf5
 module load cray-netcdf
@@ -19,7 +19,6 @@ module load nco
 module load cdo
 
 export ncdump=/opt/cray/pe/netcdf/4.9.0.3/bin/ncdump
-export LD_PRELOAD=/opt/cray/pe/gcc/12.2.0/snos/lib64/libstdc++.so.6
 
 # for grib data
 module load grib-util
