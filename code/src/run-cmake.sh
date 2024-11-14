cc757573610d93934c2421dbff1cf6c7114c54f2
mcallic2 <caitlyn.mcallister@noaa.gov>
2024-11-14 12:16:55 -0500

    Update runcmake script; removes unneeded flag setup; starts to use ifx & icx compilers if available

---
+++ code/src/run-cmake.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# adds $target variable to identify which rdhpc system you're on
+source machine-setup.sh
+
+# loads any module/packages needed for cmake build
+cd ../modulefile-setup
+source $target-setup.sh
+module list
+
+# makes and enters build directory
+cd ..
+if [ -d "build" ]; then
+   rm -rf build
+fi
+mkdir build
+cd build
+
+# build src code
+if [ $target = analysis ]; then 	# new ifx & icx compilers do not work properly with spack envs used on analysis
+  cmake .. -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icc
+else
+  cmake .. -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_C_COMPILER=icx
+fi
+
+# compile
+make
+
+# install executables in exec/ directory
+make install
+
+# back out of build directory
+cd ..
+exit
+
+
