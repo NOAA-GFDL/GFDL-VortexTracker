f74a51453712a9926c380ffe1817186199c01cd6
mcallic2 <caitlyn.mcallister@noaa.gov>
2025-01-31 14:11:41 -0500

    add missing an211 RHEL7 node to analysis array

--- code/src/machine-setup.sh
+++ code/src/machine-setup.sh
@@ -9,7 +9,7 @@ echo $HOSTNAME
 # create arrays with elements of every known login-node for each system
 analysis=("an001" "an002" "an007" "an008" "an009" "an010" "an011" "an012" "an014" \
           "an101" "an102" "an103" "an104" "an105" "an106" "an107" "an108" "an200" \
-          "an201" "an202" "an203" "an204" "an205" "an206" "an207" "an210")
+          "an201" "an202" "an203" "an204" "an205" "an206" "an207" "an210" "an211")
 
 gaea=("gaea" \
       "gaea51" "gaea52" "gaea53" "gaea54" "gaea55" "gaea56" "gaea57" "gaea58" \
