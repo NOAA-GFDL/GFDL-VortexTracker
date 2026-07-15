f5c6d958f4f0bf9ece63baa300f672bc5d6565d0
mcallic2 <caitlyn.mcallister@noaa.gov>
2026-07-15 12:21:35 -0400

    create main editable run scripts for both grib and ncdf data types

---
+++ run/scripts/grib/grib_runtrkr.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------------------
+# PLEASE READ COMMENTS CAREFULLY BEFORE EDITING THIS FILE
+# For additional instructions, you can find the documentation here:
+
+# ----- Model input data info -----
+
+# Where the data files being stored
+export datadir=
+
+# Initialization forecast date/time matching your data (yyyymmddhh format)
+export initymdh=
+
+# Add the respective forcast hours to match your data; (spaces between hrs only, no commas)
+# This will be used for temperature and height interpolation
+export fcsthrs=''
+
+# gribver=1 for GRIB1 data; gribver=2 for GRIB2 data
+export gribver=
+
+# file_sequence='multi' when there are multiple files with single frcast hour;
+# file_sequenxe='single' when all of the data is in one single file
+export file_sequence=''
+
+# ATCF name of model (4 char long)
+export atcfname=''
+# (Optional) This is descriptive and up to the developer (e.g., "6thdeg", "9km_run","1.6km_run", "15km_ens_run_member_n13", etc)
+export rundescr=''
+# (Optional) If used, it should be something that identifies the particular storm, preferably using the atcf ID.
+# For example, the GFDL model standard is to use something like "ike09l", or "two02e", etc. 
+export atcfdescr=''
+
+# ----- Additional Info -----
+
+# If you already have a tcvitals file, add path and name below.
+# If the file has not been created yet, leave this blank and the make_tcvitals.sh script will create one
+# Please note, our archived vitals information contains data up until 12/18/2025
+export tcvitals_file=
+
+# trkrtype=tracker for known storms, trkrtype=tcgen for genesis runs
+export trkrtype=''
+
+# Variables to declare the model's grid and nesting configurations
+export modtyp=''         # 'global' or 'regional'
+export nest_type=''      # 'moveable' or 'fixed'
+
+# If developer would like to include vortex tilt diagnostics;
+# 'vortex_tilt_flag' = 'y'/'n'
+# 'vortex_tilt_parm' = '<tilt_parameter_name>'
+# 'vortex_tilt_allow_thresh' = float
+export vortex_tilt_flag='y'
+export vortex_tilt_parm='zeta'
+export vortex_tilt_allow_thresh=1.0
+
+# 'need_to_*' = 'y'/'n'
+# These variables are used to detemerine if input data needs to be interpolated; i.e. input files being used
+# to run the tracker don't have data at 50mb height level (850, 750, 550, etc.).
+# If 'need_to_use_vint_or_tave' set to 'y' then interpolation code will be ran within the multi or onebig scripts.
+# User can determine if only height and/or temperature interpolation is calculated determined by the value of the following variables
+export need_to_use_vint_or_tave=''
+export need_to_interpolate_height=''
+export need_to_interpolate_temperature=''
+# -------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------------
+# DO NOT EDIT; the following code will save the information inputed and use it to run the tracker
+
+export gribdir=${PWD}
+export subdir=${PWD%/*/*}/subscripts
+export vitalsdir=${subdir}/archived_vitals
+export userinputs=${PWD%/*/*}/work/tmpfiles/userinputs.txt
+export datatype='grib'
+
+cat << EOF > ${userinputs}
+datadir='${datadir}'
+initymdh='${initymdh}'
+fcsthrs='${fcsthrs}'
+gribver='${gribver}'
+file_sequence='${file_sequence}'
+atcfname='${atcfname}'
+rundescr='${rundescr}'
+atcfdescr='${atcfdescr}'
+tcvitals_file='${tcvitals_file}'
+trkrtype='${trkrtype}'
+modtyp='${modtyp}'
+nest_type='${nest_type}'
+vortex_tilt_flag='${vortex_tilt_flag}'
+vortex_tilt_parm='${vortex_tilt_parm}'
+vortex_tilt_allow_thresh='${vortex_tilt_allow_thresh}'
+need_to_use_vint_or_tave='${need_to_use_vint_or_tave}'
+need_to_interpolate_height='${need_to_interpolate_height}'
+need_to_interpolate_temperature='${need_to_interpolate_temperature}'
+vitalsdir='${vitalsdir}'
+gribdir='${gribdir}'
+#subdir='${subdir}'
+datatype='${datatype}'
+EOF
+
+export start_trkr=${subdir}/init_trkr.sh
+source ${start_trkr}
+# -------------------------------------------------------------------------------------------------
---
+++ run/scripts/netcdf/ncdf_runtrkr.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------------------
+# PLEASE READ COMMENTS CAREFULLY BEFORE EDITING THIS FILE
+# For additional instructions, you can find the documentation here:
+
+# ----- Model input data info -----
+
+# Where the data files being stored
+export datadir=
+
+# What is the filename of the data
+export ncdf_filename=''
+
+# Initialization forecast date/time matching your data (yyyymmddhh format)
+export initymdh=
+
+# ATCF name of model (4 char long)
+export atcfname=''
+
+# ----- Additional Info -----
+
+# If you already have a tcvitals file, add path and name below.
+# If the file has not been created yet, leave this blank and the make_tcvitals.sh script will create one
+# Please note, our archived vitals information contains data up until 12/18/2025
+export tcvitals_file=
+
+# trkrtype=tracker for known storms, trkrtype=tcgen for genesis runs
+export trkrtype=''
+
+# Variables to declare the model's grid and nesting configurations
+export modtyp=''         # 'global' or 'regional'
+export nest_type=''      # 'moveable' or 'fixed'
+
+# Set 'use_land_mask' = 'y' AND 'read_serperate_land_mask_file' = 'y' if landmask file is needed.
+export use_land_mask='n'
+export read_separate_land_mask_file='n'
+# Add path to landmask file below if 'y' to above variables, leave blank if 'n'
+export ncdf_ls_mask_filename=''
+
+# If developer would like to include vortex tilt diagnostics;
+# 'vortex_tilt_flag' = 'y'/'n'
+# 'vortex_tilt_parm' = '<tilt_parameter_name>'
+# 'vortex_tilt_allow_thresh' = float
+export vortex_tilt_flag=''
+export vortex_tilt_parm=''
+export vortex_tilt_allow_thresh=''
+# -------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------------
+# DO NOT EDIT; the following code will save the information inputed and use it to run the tracker
+export ncdfdir=${PWD}
+export subdir=${PWD%/*/*}/subscripts
+export vitalsdir=${subdir}/archived_vitals
+export userinputs=${PWD%/*/*}/work/tmpfiles/userinputs.txt
+export datatype='ncdf'
+
+cat << EOF > ${userinputs}
+datadir='${datadir}'
+ncdf_filename='${ncdf_filename}'
+initymdh='${initymdh}'
+atcfname='${atcfname}'
+tcvitals_file='${tcvitals_file}'
+trkrtype='${trkrtype}'
+modtyp='${modtyp}'
+nest_type='${nest_type}'
+use_land_mask='${use_land_mask}'
+read_separate_land_mask_file='${read_separate_land_mask_file}'
+ncdf_ls_mask_filename='${ncdf_ls_mask_filename}'
+vortex_tilt_flag='${vortex_tilt_flag}'
+vortex_tilt_parm='${vortex_tilt_parm}'
+vortex_tilt_allow_thresh='${vortex_tilt_allow_thresh}'
+ncdfdir='${ncdfdir}'
+subdir='${subdir}'
+vitalsdir='${vitalsdir}'
+datatype='${datatype}'
+EOF
+
+export start_trkr=${subdir}/init_trkr.sh
+source ${start_trkr}
+# -------------------------------------------------------------------------------------------------
