4295b7c593549fdc7c6507cca60909c2542c0ab5
mcallic2 <caitlyn.mcallister@noaa.gov>
2026-07-15 13:00:37 -0400

    add subscripts

---
+++ run/subscripts/initrkr.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# print line numbers in std out
+export PS4=' line $LINENO: '
+set -x
+
+export tmpdir=${PWD%/*/*}/work/tmpfiles
+source ${tmpdir}/compileinputs.txt
+source ${tmpdir}/userinputs.txt
+
+# -------------------------------------------------------------------------------------------------
+# SECTION 3; ADDITIONAL VARIABLE DECLARINING, DOES NOT NEED TO BE EDITED BY USER
+
+# slice init date/time to use later in script
+export pdy=`     echo ${initymdh} | cut -c1-8`
+export yyyy=`    echo ${initymdh} | cut -c1-4`
+export cc=`      echo ${initymdh} | cut -c1-2`
+export yy=`      echo ${initymdh} | cut -c3-4`
+export mm=`      echo ${initymdh} | cut -c5-6`
+export dd=`      echo ${initymdh} | cut -c7-8`
+export hh=`      echo ${initymdh} | cut -c9-10`
+export ymdh=${initymdh}
+
+# set date stamp vars
+export date_stamp=$(date +"%a %b %d %H:%M:%S %Z %Y")
+export today_stamp=$(date +"%m.%d-%H.%M%p")
+
+# set wdir path
+export wdir=${workdir}/${datatype}/${atcfname}
+if [ ! -d ${wdir} ]; then mkdir -p ${wdir}; fi
+
+set +x
+# -------------------------------------------------------------------------------------------------
+# SECTION 4; INVOKE SCRIPTS
+
+# load env modules 
+source ${compdir}/system-envs/${compiler}/${system}.sh
+
+# list modules
+echo -e " "
+module list
+
+if [ ${datatype} = 'ncdf' ]; then
+  export atmosvars=${PWD}/atmos_${datatype}_vars.sh
+  source ${atmosvars}
+  if [ ${usercheck} != 'CHECKED' ]; then
+    echo -e " "
+	  echo -e "** USER CHECK FAILED**"
+	  echo -e "Please return to: "
+    echo -e "\t${atmosvars}"
+    echo -e "\tand ensure all variables are declared accordingly"
+    echo -e " "
+    echo -e "Documentation and additional instructions can be found here: "
+		echo -e "\t<add link here when ready>"
+		echo -e " "
+		echo -e "If a you are experiencing other complications please open an issue via GitHub"
+		echo -e "Issues can be created here: https://github.com/NOAA-GFDL/GFDL-VortexTracker/issues/new"
+		echo -e " "
+	  echo -e " "
+	  exit 0
+  fi  
+fi
+
+source ${subdir}/${datatype}_vars.sh
+
+export vitalsdir=${subdir}/archived_vitals
+source ${subdir}/make_tcvitals.sh
+
+source ${subdir}/namelist.sh 
+
+source ${subdir}/IOfiles.sh
+
+# print tracker set up is finished
+echo "TRACKER SET UP FINISHED"
+
+# -------------------------------------------------------------------------------------------------
+# SECTION 5; EXECUTE TRACKER SOURCE CODE
+
+#echo "INITIALIZE TRACKER EXECUTABLE"
+#echo "Running tracker for ${atcfname} at ${hh}z at ${date_stamp}"
+
+#${execdir}/gettrk.x
+#export gettrk_rcc=$?
+
+#echo "After tracker source code run  ---> ${date_stamp}"
+#echo "Return code from tracker= gettrk_rcc= ${gettrk_rcc}"
+
+# add print statement if tracker completed successfully
+#if [ ${gettrk_rcc} -gt 0 ]; then
+#  echo "TRACKER DID NOT RUN TO COMPLETION"
+#  exit 1
+#else
+#  echo "TRACKER RAN SUCCESSFULLY"
+#fi
+
+exit 0
